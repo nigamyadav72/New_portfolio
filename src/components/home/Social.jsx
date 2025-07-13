8147ab2d7c06f99996765ed8e821f1caf0736db2
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2025-07-13 08:32:38 +0545

    minor adjustment

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Social = () => {
   return (
     <div className='home__social'>
-        <a href="https://www.instagram.com/" className='home__social-icon' target='_blank'>
+        <a href="https://www.instagram.com/nigamyadav72/" className='home__social-icon' target='_blank'>
             <i class="uil uil-instagram"></i>
         </a>
 
 
-        <a href="https://www.linkedin.com/" className='home__social-icon' target='_blank'>
+        <a href="https://www.linkedin.com/in/nigam-yadav-099447199/" className='home__social-icon' target='_blank'>
             <i class="uil uil-linkedin-alt"></i>
         </a>
         
-        <a href="https://github.com/" className='home__social-icon' target='_blank'>
+        <a href="https://github.com/nigamyadav72/" className='home__social-icon' target='_blank'>
             <i class="uil uil-github-alt"></i>
         </a>
       
