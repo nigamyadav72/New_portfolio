1b51c874fdecbe1556772c6fd53dc7e5e94cc49a
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2025-07-30 17:54:35 +0545

    resume updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./about.css";
 import AboutImg from "../../assets/mine.jpeg";
-import CV from "../../assets/John-Cv.pdf";
+import CV from "../../assets/nigam-cv.pdf";
 import Info from './Info';
 
 const About = () => {
