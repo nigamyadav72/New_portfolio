fc431345345f0c2a1ca7abfde598316b0c7c0825
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2025-08-02 23:21:25 +0545

    portfolio removed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -67,7 +67,7 @@ const Header = () => {
                 Services
               </a>
             </li>
-            <li className="nav__item">
+            {/* <li className="nav__item">
               <a
                 href="#portfolio"
                 onClick={() => setActiveNav("#portfolio")}
@@ -80,7 +80,7 @@ const Header = () => {
                 <i className="uil uil-scenery nav__icon"></i>
                 Portfolio
               </a>
-            </li>
+            </li> */}
             <li className="nav__item">
               <a
                 href="#contact"
